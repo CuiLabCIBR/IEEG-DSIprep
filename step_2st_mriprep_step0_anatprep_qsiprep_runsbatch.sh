388609404d7532f0e80b4b211d48cfc067797787
xlzhope <xlzhope@foxmail.com>
2022-10-28 10:18:51 +0800

    Delete step_2st_mriprep_step0_anatprep_qsiprep_runsbatch.sh

--- step_2st_mriprep_step0_anatprep_qsiprep_runsbatch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for subj in {0002..0112}
-do
-echo ""
-echo "Running anatprep using qsiprep on participant: sub-$subj"
-echo ""
-sbatch step_2st_mriprep_step0_anatprep_qsiprep.sh $subj
-done
