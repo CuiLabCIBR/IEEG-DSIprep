494d15e3a330c8c51646cb5416b0833e7a889cf0
xlzhope <xlzhope@foxmail.com>
2022-10-28 10:20:35 +0800

    Delete step_2st_mriprep_step3_dsiprep_qsiprep_runsbatch.sh

--- step_2st_mriprep_step3_dsiprep_qsiprep_runsbatch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for subj in {0002..0112}
-do
-echo ""
-echo "Running dsiprep using qsiprep on participant: sub-$subj"
-echo ""
-sbatch step_2st_mriprep_step3_dsiprep_qsiprep.sh $subj
-done
